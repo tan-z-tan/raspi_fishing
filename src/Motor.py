928f476287695c5e54af6e9e6752057faec00da5
Makoto Tanji <tanji.makoto@gmail.com>
2016-02-11 23:15:56 +0900

    Motor class

---
+++ src/Motor.py
@@ -0,0 +1,44 @@
+class Motor:
+	def __init__(self):
+
+	def init_gpio(self):
+	    GPIO.setmode(GPIO.BCM)
+	    
+	    channel_list = [14, 15, 18]
+	    GPIO.setup(channel_list, GPIO.OUT)
+
+	def stop(self):
+	    GPIO.output(14, False)
+	    GPIO.output(15, False)
+	    GPIO.output(18, False)
+
+	def rotate_right(self, duration):
+		stop()
+		GPIO.output(14, True)
+		GPIO.output(15, False)
+		GPIO.output(18, True)
+		time.sleep(duration)
+		stop()
+
+	def rotate_left(self, duration):
+	    stop()
+	    GPIO.output(14, False)
+	    GPIO.output(15, True)
+	    GPIO.output(18, True)
+	    time.sleep(duration)
+	    stop()
+
+	def cleanup(self):
+	    GPIO.cleanup()
+
+
+if __name__ == "__main__":
+	motor = Motor()
+
+	motor.init_gpio()
+
+	motor.rotate_right(2)
+
+	motor.rotate_left(2)
+
+	motor.cleanup()
