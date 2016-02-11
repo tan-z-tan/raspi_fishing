20d83256f7df18717dd8a4da1549bc33863bb435
Makoto Tanji <tanji.makoto@gmail.com>
2016-02-11 14:19:59 +0000

    Fix motor class

--- src/Motor.py
+++ src/Motor.py
@@ -1,44 +1,44 @@
+import RPi.GPIO as GPIO
+import time
+
 class Motor:
 	def __init__(self):
-
+                self.init_gpio()
+                
 	def init_gpio(self):
-	    GPIO.setmode(GPIO.BCM)
+                GPIO.setmode(GPIO.BCM)
 	    
-	    channel_list = [14, 15, 18]
-	    GPIO.setup(channel_list, GPIO.OUT)
+                channel_list = [14, 15, 18]
+                GPIO.setup(channel_list, GPIO.OUT)
 
 	def stop(self):
-	    GPIO.output(14, False)
-	    GPIO.output(15, False)
-	    GPIO.output(18, False)
+                GPIO.output(14, False)
+                GPIO.output(15, False)
+                GPIO.output(18, False)
 
 	def rotate_right(self, duration):
-		stop()
+		self.stop()
 		GPIO.output(14, True)
 		GPIO.output(15, False)
 		GPIO.output(18, True)
 		time.sleep(duration)
-		stop()
+		self.stop()
 
 	def rotate_left(self, duration):
-	    stop()
-	    GPIO.output(14, False)
-	    GPIO.output(15, True)
-	    GPIO.output(18, True)
-	    time.sleep(duration)
-	    stop()
+                self.stop()
+                GPIO.output(14, False)
+                GPIO.output(15, True)
+                GPIO.output(18, True)
+                time.sleep(duration)
+                self.stop()
 
 	def cleanup(self):
-	    GPIO.cleanup()
-
+                GPIO.cleanup()
 
 if __name__ == "__main__":
 	motor = Motor()
 
-	motor.init_gpio()
-
+        motor.init_gpio()
 	motor.rotate_right(2)
-
 	motor.rotate_left(2)
-
 	motor.cleanup()
